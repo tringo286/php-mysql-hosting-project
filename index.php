c40e8c4fb069cad8387779b48499284cfce0f666
Tri Ngo <tringo831@gmail.com>
2025-09-25 16:20:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-phpinfo();
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
